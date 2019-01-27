bec9f93086c7400fe497a7e5fbe4f49581e6d061
Heltonss <Helton@heltonsouza.com.br>
2019-01-27 18:14:39 -0200

    adicona metodos get e post com axios

--- src/App.js
+++ src/App.js
@@ -1,35 +1,119 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './css/layout/side-menu.css';
+import axios from 'axios';
+import InputCustomizado from './componentes/InputCustomizado';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { lista: [], nome: '', email: '', senha: '' };
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
+
+  }
+
+  componentDidMount() {
+    this.getAutores();
+  }
+
+  getAutores() {
+    axios.get('http://localhost:5000/autores')
+      .then(res => this.setState({ lista: res.data }))
+      .catch(err => console.log(err));
+  }
+
+  enviaForm(event) {
+    event.preventDefault();
+    axios.post('http://localhost:5000/autores', {
+      nome: this.state.nome,
+      email: this.state.email,
+      senha: this.state.senha
+    })
+      .then(res => this.getAutores())
+      .catch(err => console.log(err))
+  }
+
+  setNome(evento) {
+    this.setState({ nome: evento.target.value });
+  }
+
+  setEmail(evento) {
+    this.setState({ email: evento.target.value });
+  }
+
+  setSenha(evento) {
+    this.setState({ senha: evento.target.value });
+  }
+
   render() {
     return (
-    <div id="layout">
-      <div id="menu">
-        <div class="pure-menu">
-            <a class="pure-menu-heading" href="#">Company</a>
+      <div id="layout">
 
-            <ul class="pure-menu-list">
-                <li class="pure-menu-item"><a href="#" class="pure-menu-link">Home</a></li>
-                <li class="pure-menu-item"><a href="#" class="pure-menu-link">About</a></li>
+        <a href="#menu" id="menuLink" className="menu-link">
 
-                <li class="pure-menu-item menu-item-divided pure-menu-selected">
-                    <a href="#" class="pure-menu-link">Services</a>
-                </li>
+          <span></span>
+        </a>
 
-                <li class="pure-menu-item"><a href="#" class="pure-menu-link">Contact</a></li>
+        <div id="menu">
+          <div className="pure-menu">
+            <a className="pure-menu-heading" href="#">Company</a>
+
+            <ul className="pure-menu-list">
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+
+
             </ul>
+          </div>
         </div>
-    </div>
 
-    <div id="main">
-        <div class="header">
-            <h1>my library of books</h1>
+        <div id="main">
+          <div className="header">
+            <h1>Cadastro de Autores</h1>
+          </div>
+          <div className="content" id="content">
+            <div className="pure-form pure-form-aligned">
+              <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm}>
+                <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome" />
+                <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email" />
+                <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha" />
+                <div className="pure-control-group">
+                  <label></label>
+                  <button type="submit" className="pure-button pure-button-primary">Gravar</button>
+                </div>
+              </form>
+
+            </div>
+            <div>
+              <table className="pure-table">
+                <thead>
+                  <tr>
+                    <th>Nome</th>
+                    <th>email</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    this.state.lista.map(autor => {
+                      return (
+                        <tr key={autor.id}>
+                          <td>{autor.nome}</td>
+                          <td>{autor.email}</td>
+                        </tr>
+                      );
+                    })
+                  }
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
-    </div>
-    </div>
+
+      </div>
     );
   }
 }
